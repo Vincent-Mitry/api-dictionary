4a55ddd96813d0889d651d159f38bd8d3160d1b6
Vincent Mitry <vinz.mitry@gmail.com>
2021-11-24 14:46:41 +0100

    récupération des données de l'api et affichage du titre et des definitions dans le DOM

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log('hello');
+document.querySelector('#form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const wordToSearch = document.querySelector('#word').value;
+
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordToSearch}`)
+        .then(Response => Response.json()
+        .then(Json => {
+            console.log(Json);
+            /*****************************************************************
+            * Data from Json Object
+            *****************************************************************/
+            const word = Json[0].word;
+            const meanings = Json[0]['meanings'];
+
+            console.log(meanings[0]);
+            /*****************************************************************
+            * Display the word title in DOM
+            *****************************************************************/
+            document.querySelector('#word-data').innerHTML = word;
+            
+            /*****************************************************************
+            * Display the definitions list in DOM
+            *****************************************************************/
+            const definitionUlElement = document.querySelector('#definition-list');
+            // On vide la liste des définitions
+            emptyElement(definitionUlElement);
+            // On insère chaque définition dans la liste de puces (#definition-list) en créant un 'li' pour chaque élément
+            for (const meaning of meanings) {
+                let definitionLiElement = document.createElement('li');
+                definitionLiElement.innerHTML = meaning.definitions[0].definition;
+                definitionUlElement.append(definitionLiElement);
+            }
+        }))
+    
+})
+
+/**
+ * Empties a DOM element
+ * @param {*} element 
+ */
+function emptyElement(element) {
+    while(element.firstElementChild) {
+       element.firstElementChild.remove();
+    }
+  }
+  
