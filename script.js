dae63f0d1fdc8a791d8d3bc641d667d30231236f
Vincent Mitry <vinz.mitry@gmail.com>
2021-11-28 20:09:42 +0100

    gestion et affichage des erreurs  - affichage carte des définitions

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 const inputWordElement = document.querySelector('#word');
+const wordTitle = document.querySelector('#word-data');
 const definitionDivElement = document.querySelector('#definition-data');
 
 function fetchData() {
-
     const wordToSearch = inputWordElement.value;
 
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordToSearch}`)
-    .then(Response => Response.json()
-    .then(Json => {
-        if(!Response.ok){
-            throw Json;
-        }
-        
-        displayWordInfo(Json);
-    }))
-    .catch(error => {
-        displayErrors(error);
-    })
+    if(wordToSearch.length > 0){
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordToSearch}`)
+        .then(Response => Response.json()
+        .then(Json => {
+            if(!Response.ok){
+                throw Json;
+            }
+            
+            displayWordInfo(Json);
+        }))
+        .catch(error => {
+                displayErrors(error);
+        })
+    }
 }
-
 
 function displayWordInfo(data){
     const word = data[0].word;
@@ -26,16 +27,21 @@ function displayWordInfo(data){
     /*****************************************************************
      * Display the word title in DOM
      *****************************************************************/
-    document.querySelector('#word-data').innerHTML = word;
+    wordTitle.innerHTML = word;
     /*****************************************************************
      * Display the definitions list in DOM
      *****************************************************************/
     emptyDataElement();
     // On insère chaque définition dans la div '#definition-list' (on créé un élément 'p' pour chaque définition) 
     for (const meaning of meanings) {
+        const cardDiv = document.createElement('div');
+        cardDiv.className = "card bg-secondary p-2 mb-1";
+        definitionDivElement.append(cardDiv);
+
         const definitionPElement = document.createElement('p');
         definitionPElement.innerHTML = meaning.definitions[0].definition;
-        definitionDivElement.append(definitionPElement);
+        definitionPElement.className = "m-0";
+        cardDiv.append(definitionPElement);
         
         const partOfSpeechSpanElement = document.createElement('span');
         partOfSpeechSpanElement.className = "fst-italic";
@@ -58,23 +64,30 @@ function emptyDataElement() {
  * @param {*} data 
  */
 function displayErrors(data) {
-    const errorTitle = data.title;
-    const errorMessage = data.message;
+    let errorTitle;
+    let errorMessage;
+
+    if(typeof(data.title) !== 'undefined'){
+        errorTitle = data.title;
+        errorMessage = data.message;
+    } else {
+        errorTitle = "An error has occured";
+        errorMessage = `Oups! Something went wrong... Please try again later or contact the support.<br>
+                        Error message: <span  class="fst-italic">"${data}"</span>`;
+    }
     
     emptyDataElement();
     
-    document.querySelector('#word-data').innerHTML = errorTitle;
+    wordTitle.innerHTML = errorTitle;
+
     const errorMessagePElement = document.createElement('p');
     errorMessagePElement.innerHTML = errorMessage;
+    errorMessagePElement.className = "m-0";
     definitionDivElement.append(errorMessagePElement);
 }
 
 document.querySelector('#form').addEventListener('submit', (e) => {
-    if(inputWordElement.value !== ""){
         e.preventDefault();
         fetchData();
-    } else {
-        e.preventDefault();
-    }
 })
 
